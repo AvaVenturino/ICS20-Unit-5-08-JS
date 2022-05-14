f8f64208cb5a29766105045f46315a89623beea4
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-05-14 00:42:18 +0000

    changed spacing

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function myButtonClicked() {
   const counter2 = parseInt(document.getElementById("counter2").value);
   var addedNumber = 0;
   var answer = counter1;
-  var remainder = 0
+  var remainder = 0;
 
   if (counter1 > 0 && counter2 > 0) {
     while (answer >= counter2) {
@@ -29,13 +29,13 @@ function myButtonClicked() {
       answer = answer - counter2;
     }
   }
-  
+
   if (answer == 0) {
-    remainder = 0
+    remainder = 0;
   } else if (answer != 0) {
-    remainder = answer + counter2
+    remainder = answer + counter2;
   }
-  
+
   document.getElementById("answer").innerHTML =
     counter1 + " ÷ " + counter2 + " = " + addedNumber + " The remainder is " + remainder;
 }
